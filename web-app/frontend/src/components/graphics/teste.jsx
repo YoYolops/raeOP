9bdc74bbf2332f37008b86a1d5405c164cafe1f8
YoYolops <yohanhxh@gmail.com>
2020-11-24 18:39:13 -0300

    adicionando primeiro gráfico

--- web-app/frontend/src/components/graphics/teste.jsx
+++ web-app/frontend/src/components/graphics/teste.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Bar, HorizontalBar } from 'react-chartjs-2';
+import { HorizontalBar } from 'react-chartjs-2';
 import './teste.css'
 
 const BarChart = () => {
@@ -8,84 +8,95 @@ const BarChart = () => {
             <HorizontalBar 
                 data = {{
                     labels: [
-                        "Administração",
-                        "Agronomia",
-                        "Arquitetura e Urbanismo",
-                        "Arte e Mídia",
-                        "Ciência da Computação",
-                        "Ciências Biológicas",
-                        "Ciências Contábeis",
-                        "Ciências Econômicas",
-                        "Ciências Sociais",
-                        "Comunicação Social",
-                        "Design",
-                        "Direito",
-                        "Enfermagem",
-                        "Engenharia Agrícola",
-                        "Engenharia Ambiental",
-                        "Engenharia Civil",
-                        "Engenharia de Alimentos",
-                        "Engenharia de Biossistemas",
-                        "Engenharia de Biotecnologia e Bioprocessos",
-                        "Engenharia de Materiais",
-                        "Engenharia de Minas",
-                        "Engenharia de Petróleo",
-                        "Engenharia de Produção",
-                        "Engenharia Elétrica",
-                        "Engenharia Florestal",
-                        "Engenharia Mecânica",
-                        "Engenharia Química",
-                        "Estatística",
-                        "Farmácia",
-                        "Filosofia",
-                        "Física",
-                        "Geografia",
-                        "Gestão Pública",
-                        "História",
-                        "Letras",
-                        "Matemática",
-                        "Medicina",
-                        "Medicina Veterinária",
-                        "Meteorologia",
-                        "Nutrição",
-                        "Odontologia",
-                        "Pedagogia",
-                        "Psicologia",
-                        "Química"
-                    ],
+                        'Direito', 'Engenharia Civil', 'Ciência da Computação',
+                        'Engenharia Elétrica', 'Geografia', 'Ciências Biológicas',
+                        'Nutrição', 'Farmácia', 'Enfermagem', 'Matemática', 'História',
+                        'Medicina', 'Design', 'Química', 'Administração',
+                        'Engenharia de Petróleo', 'Arte e Mídia', 'Letras', 'Estatística',
+                        'Engenharia Ambiental', 'Psicologia', 'Engenharia de Alimentos',
+                        'Engenharia Mecânica', 'Física', 'Filosofia', 'Engenharia Florestal',
+                        'Engenharia Agrícola', 'Engenharia Química', 'Engenharia de Produção',
+                        'Pedagogia', 'Ciências Sociais', 'Arquitetura e Urbanismo', 'Agronomia',
+                        'Odontologia', 'Meteorologia', 'Medicina Veterinária',
+                        'Ciências Econômicas', 'Engenharia de Minas', 'Engenharia de Materiais',
+                        'Engenharia de Biotecnologia e Bioprocessos', 'Ciências Contábeis',
+                        'Gestão Pública', 'Engenharia de Biossistemas', 'Comunicação Social'
+                ],
 
                     datasets: [
                         {
-                            label: "Nº de Respondentes",
+                            label: "Quantidade",
                             data: [
-                                38,5, 5, 34, 114, 92, 2, 3, 5, 1,
-                                53, 138, 73, 8, 25, 128, 24, 1, 2, 2,
-                                2, 37, 7, 112, 8, 12, 7, 25, 82, 9,
-                                10, 99, 1, 66, 25, 71, 63, 4, 4, 90,
-                                4, 6, 24, 46
+                                138, 128, 114, 112, 99, 92, 90, 82, 73, 71, 66, 63, 53, 46,
+                                38, 37, 34, 25, 25, 25, 24, 24, 12, 10, 9, 8, 8, 7, 7, 6, 5,
+                                5, 5, 4, 4, 4, 3, 2, 2, 2, 2, 1, 1, 1
                             ],
-                            backgroundColor: '#FFBF00',
-                            borderRadius: 3,
-                            barThickness: 10
-                        }
+                            backgroundColor: '#EEAD2D',
+                            barThickness: 'flex',
+                        },
+
+                        {
+                            label: "Porcentagem",
+                            data: [
+                                8.81, 8.17, 7.28, 7.15, 6.32, 5.87, 5.74, 5.23, 4.66, 4.53, 4.21, 4.02, 3.38,
+                                2.94, 2.43, 2.36, 2.17, 1.6, 1.6, 1.6, 1.53, 1.53, 0.77, 0.64, 0.57, 0.51, 0.51,
+                                0.45, 0.45, 0.38, 0.32, 0.32, 0.32, 0.26, 0.26, 0.26, 0.19, 0.13, 0.13, 0.13, 0.13,
+                                0.06, 0.06, 0.06
+                            ],
+                            backgroundColor: '#00ff08',
+                            barThickness: 'flex',
+                        },
                     ],
 
-                
-
+            
                 }}
-                height = {400}
-                width = {600}
+                height = {1000}
+                width = {400}
                 options = {{
                     maintainAspectRatio: false,
+
+                    layout: {
+                        padding: 60,
+                    },
+
+                    legend: {
+                        display: true,
+                        labels: {
+                            fontSize: 15,
+                        },
+                    },
+
+                    title: {
+                        display: true,
+                        padding: 10,
+                        text: "Nº de Respondentes",
+                        fontSize: 25,
+                    },
+
                     scales: {
                         yAxes:[
                             {
+                                gridLines: {
+                                    color: "rgba(0, 0, 0, 0)",
+                                },
+
                                 ticks: {
                                     beginAtZero: true
                                 },
                             },
                         ],
+
+                        xAxes:[
+                            {
+                                display: false,
+                                gridLines: {
+                                    color: "rgba(0,0,0,0)",
+                                },
+                            }
+                        ],
+
                     },
+
                 }}
             />
         </div>
