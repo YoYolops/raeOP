0468b5aced50ab6819fcf0f41abbbc277c254dae
YoYolops <yohanhxh@gmail.com>
2020-11-25 21:37:59 -0300

    removendo linha index.css que bugava estilização da navbar

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import DefaultPage from './components/DefaultPage';
+import DefaultPage from './components/DefaultPage/index.js';
 import Perfil from './components/Perfil.jsx';
 
 function App() {
   return (
     <>
       <Switch>
-        <Route path="/main" component={Perfil} />
+        <Route path="/main" component={DefaultPage} />
       </Switch>
     </>
   );
