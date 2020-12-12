cc0a6ca12c013e9c6e7e487bb7a78f7cf3449fca
YoYolops <yohanhxh@gmail.com>
2020-12-11 22:29:46 -0300

    adicionando estado aos <li /> da navbar

--- web-app/frontend/src/components/NavBar/Dropdown/DropdownItem/index.js
+++ web-app/frontend/src/components/NavBar/Dropdown/DropdownItem/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './DropdownItem.css';
+import { NavLink } from 'react-router-dom';
 
 const DropdownItem = (props) => {
     return (
-        <li className='listItem'><a href={props.href} className='drop-link'>{props.children}</a></li>
-    )
-}
+        <li className='listItem'>
+            <a href={props.href} className='drop-link'>
+                {props.children}
+            </a>
+        </li>
+    );
+};
 
 export default DropdownItem;
--- web-app/frontend/src/components/NavBar/index.js
+++ web-app/frontend/src/components/NavBar/index.js
@@ -6,7 +6,16 @@ import DropdownItem from './Dropdown/DropdownItem';
 
 class Navbar extends Component {
 
-    state = { bars_clicked: false, profile_clicked: false, rae_clicked: false, teacher_clicked: false, institution_clicked: false }
+    constructor(props) {
+        super(props)
+        this.state = {
+            bars_clicked: false,
+            profile_clicked: false,
+            rae_clicked: false,
+            teacher_clicked: false,
+            institution_clicked: false
+        }
+    }
 
     handleClickBar = () => {
         this.setState({ bars_clicked: !this.state.bars_clicked })
@@ -34,12 +43,12 @@ class Navbar extends Component {
 
                     <li className='listItem'><a className='nav-link' href='#overview'>Overview</a></li>
 
-                    <li className='listItem'><a className='nav-link' href='#student_profile' onClick={this.handleClickProfile}>Perfil do Respondente</a></li>
+                    <li className='listItem'><a href='#student_profile' onClick={this.handleClickProfile} className={this.state.profile_clicked ? 'nav-link-active' : 'nav-link'}>Perfil do Respondente</a></li>
                     <Dropdown className={this.state.profile_clicked ? 'drop-menu active' : 'drop-menu'}>
                         <DropdownItem href='#quant_students'>Quantidade de Respondentes</DropdownItem>
                     </Dropdown>
 
-                    <li className='listItem'><a className='nav-link' href='#about_rae' onClick={this.handleClickRae}>Sobre RAE</a></li>
+                    <li className='listItem'><a href='#about_rae' onClick={this.handleClickRae} className={this.state.rae_clicked ? 'nav-link-active' : 'nav-link'}>Sobre RAE</a></li>
                     <Dropdown className={this.state.rae_clicked ? 'drop-menu active' : 'drop-menu'}>
                         <DropdownItem href='#vac_offered'>Vagas Ofertadas</DropdownItem>
                         <DropdownItem href='#courses_taken'>Disciplinas Cursadas</DropdownItem>
@@ -51,7 +60,7 @@ class Navbar extends Component {
                         <DropdownItem href='#performance impact'>Impacto das Dificuldades</DropdownItem>
                     </Dropdown>
 
-                    <li className='listItem'><a className='nav-link' href='#about_teacher' onClick={this.handleClickTeacher}>Sobre Professores</a></li>
+                    <li className='listItem'><a href='#about_teacher' onClick={this.handleClickTeacher} className={this.state.teacher_clicked ? 'nav-link-active' : 'nav-link'}>Sobre Professores</a></li>
                     <Dropdown className={this.state.teacher_clicked ? 'drop-menu active' : 'drop-menu'}>
                         <DropdownItem href='#teacher_tolerance'>Tolerância dos Professores</DropdownItem>
                         <DropdownItem href='#performance_teachers'>Atuação dos Professores</DropdownItem>
@@ -59,14 +68,15 @@ class Navbar extends Component {
                         <DropdownItem href='#teacher_requirement'>Exigência dos Professores</DropdownItem>
                         <DropdownItem href='#volume_activities'>Volume de Atividades no RAE</DropdownItem>
                         <DropdownItem href='#about_activities'>Sobre essas Atividades</DropdownItem>
-
                     </Dropdown>
 
-                    <li className='listItem'><a className='nav-link' href='#about_institution' onClick={this.handleClickInstitution}>Sobre Instituição</a></li>
+                    <li className='listItem'><a href='#about_institution' onClick={this.handleClickInstitution} className={this.state.institution_clicked ? 'nav-link-active' : 'nav-link'}>Sobre Instituição</a></li>
                     <Dropdown className={this.state.institution_clicked ? 'drop-menu active' : 'drop-menu'}>
                         <DropdownItem href='#ufcg_performance'>Avaliação da UFCG</DropdownItem>
                         <DropdownItem href='#coordination_performance'>Desempenho das Coordenações</DropdownItem>
                     </Dropdown>
+
+                    <li className='listItem'><a className='nav-link' href='#metodologia'>Metodologia</a></li>
                 </ul>
             </nav>
         )
